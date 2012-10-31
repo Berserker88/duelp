0ebf449c04584b5d10b110616e59584dfc9474a2
FlorianNB <FlorianNB@10.12.40.240>
2012-10-31 14:11:48 +0100

    bla

--- src/main.java
+++ src/main.java
@@ -2,10 +2,6 @@
 public class main {
 //akis kommentar ;D
 	//dem jannis sein push...
-<<<<<<< HEAD
 	// Flos Kommentar2
-=======
-	// akis 2. Kommentar
->>>>>>> branch 'master' of https://github.com/Berserker88/duelp.git
 
 }
