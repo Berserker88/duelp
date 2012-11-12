ed7fe28a097f5228a87cd6bca030a1f8a618326b
aki18 <nitroaki@hotmail.com>
2012-11-12 12:23:33 +0100

    Kommentar hinzugef�gt

--- app/src/de/jasiflak/duelp/Lernplan.java
+++ app/src/de/jasiflak/duelp/Lernplan.java
@@ -8,5 +8,6 @@ public class Lernplan extends Activity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lernplan_layout);
+		//test
 	}
 }
