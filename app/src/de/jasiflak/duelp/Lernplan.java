cd13ec90103c6cdde7c87cbd6ad5761e77161c65
aki18 <nitroaki@hotmail.com>
2012-11-13 11:51:31 +0100

    ++++Lernplan_layout.xml++++ -TextView hinzugef�gt -ListView hinzugef�gt
    +++Lernplan.java+++
    -Instanziieren einer Liste
    -bef�llen der Liste mittels einer For-Schleife mit test-Daten
    -generieren eines �ListAdapter� f�r die �ListView�
    -ListView aus layout holen und ihr den Adapter �bergeben

--- app/src/de/jasiflak/duelp/Lernplan.java
+++ app/src/de/jasiflak/duelp/Lernplan.java
@@ -1,13 +1,37 @@
 package de.jasiflak.duelp;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class Lernplan extends Activity{
-	public void onCreate(Bundle savedInstanceState){
+public class Lernplan extends Activity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lernplan_layout);
-		//test
+
+		
+		//erstellen einer Liste
+		List<String> valueList = new ArrayList<String>();
+		
+		//
+		for (int i = 1; i <= 10; i++)
+		{
+			valueList.add(i+".Termin");
+		}
+		
+		ListAdapter adapter = new ArrayAdapter<String> (getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+		
+	    final ListView lv = (ListView)findViewById(R.id.listView1);
+
+	    lv.setAdapter(adapter);
 	}
+	
+
 }
