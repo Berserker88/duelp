837eabdd0e93210ba620cc8f88271d295ca75152
aki18 <nitroaki@hotmail.com>
2012-11-13 16:42:29 +0100

    +++Lernplan.java+++ - Neue testdaten erstellt mittels Hashmap - f�llen der liste mittels einer For-Schleife und Iterieren der Hashmap - L�schen der alten testdaten

--- app/src/de/jasiflak/duelp/Lernplan.java
+++ app/src/de/jasiflak/duelp/Lernplan.java
@@ -2,7 +2,10 @@ package de.jasiflak.duelp;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -11,6 +14,7 @@ import android.widget.ListView;
 
 public class Lernplan extends Activity
 {
+	
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
@@ -20,10 +24,25 @@ public class Lernplan extends Activity
 		//Instanziieren einer Liste, erstellen einer Liste
 		List<String> valueList = new ArrayList<String>();
 		
+		//Testdaten
+		Map<Integer, String[]> testdaten = new HashMap<Integer, String[]>();
+		String[] eintrag1 = {"15.11.12","MoIT","10:00","18:00","Flo","ja"};
+		    String[] eintrag2 = {"16.11.12","ITS","10:00","18:00","Flo","ja"};
+		    String[] eintrag3 = {"17.11.12","VSY","10:00","18:00","Flo","ja"};
+		    String[] eintrag4 = {"19.11.12","MoIT","10:00","18:00","Flo","ja"};
+		    String[] eintrag5 = {"23.11.12","MoIT","10:00","18:00","Flo","ja"};
+		    testdaten.put(1, eintrag1);
+		    testdaten.put(2, eintrag2);
+		    testdaten.put(3, eintrag3);
+		    testdaten.put(4, eintrag4);
+		    testdaten.put(5, eintrag5);
+		
+		    
 		//befüllen der Liste
-		for (int i = 1; i <= 100; i++)
+		for (Map.Entry<Integer, String[]> entry: testdaten.entrySet())
 		{
-			valueList.add(i+".Termin");
+			String[] temp = entry.getValue();//temp-Array
+			valueList.add(temp[0]+" "+temp[1]+temp[2]+temp[3]+temp[4]+temp[5]);
 		}
 		
 		//generieren eines „ListAdapter“ für die „ListView“
@@ -32,6 +51,7 @@ public class Lernplan extends Activity
 		//ListView aus layout holen und ihr den Adapter übergeben
 	    final ListView lv = (ListView)findViewById(R.id.listView1);
 	    lv.setAdapter(adapter);
+	    
 	}
 	
 
