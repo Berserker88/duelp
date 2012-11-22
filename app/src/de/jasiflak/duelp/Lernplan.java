8ba821f9ea2a04e4072ed8b430d21119b545a4b9
aki18 <nitroaki@hotmail.com>
2012-11-22 12:30:59 +0100

    Mit der Formatierung gespielt. Keine wirklichen �nderungen

--- app/src/de/jasiflak/duelp/Lernplan.java
+++ app/src/de/jasiflak/duelp/Lernplan.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
@@ -26,23 +27,23 @@ public class Lernplan extends Activity
 		
 		//Testdaten
 		Map<Integer, String[]> testdaten = new HashMap<Integer, String[]>();
-		String[] eintrag1 = {"15.11.12","MoIT","10:00","18:00","Flo","ja"};
-		    String[] eintrag2 = {"16.11.12","ITS","10:00","18:00","Flo","ja"};
-		    String[] eintrag3 = {"17.11.12","VSY","10:00","18:00","Flo","ja"};
-		    String[] eintrag4 = {"19.11.12","MoIT","10:00","18:00","Flo","ja"};
-		    String[] eintrag5 = {"23.11.12","MoIT","10:00","18:00","Flo","ja"};
-		    testdaten.put(1, eintrag1);
-		    testdaten.put(2, eintrag2);
-		    testdaten.put(3, eintrag3);
-		    testdaten.put(4, eintrag4);
-		    testdaten.put(5, eintrag5);
-		
+		String[] eintrag1 = {"15.11.12","MoIT","13:00","16:00","Flo","nein"};
+	    String[] eintrag2 = {"16.11.12","ITS","14:00","19:00","Simon","ja"};
+	    String[] eintrag3 = {"17.11.12","VSY","12:00","17:30","Jannis","nein"};
+	    String[] eintrag4 = {"19.11.12","SWE","10:30","15:30","Aki","ja"};
+	    String[] eintrag5 = {"23.11.12","EZS","11:00","20:30","Uni","nein"};
+	    testdaten.put(1, eintrag1);
+	    testdaten.put(2, eintrag2);
+	    testdaten.put(3, eintrag3);
+	    testdaten.put(4, eintrag4);
+	    testdaten.put(5, eintrag5);
+	
 		    
 		//befüllen der Liste
 		for (Map.Entry<Integer, String[]> entry: testdaten.entrySet())
 		{
 			String[] temp = entry.getValue();//temp-Array
-			valueList.add(temp[0]+" "+temp[1]+temp[2]+temp[3]+temp[4]+temp[5]);
+			valueList.add(temp[0]+"	"+temp[1]+" "+temp[2]+" -"+temp[3]+"\nOrt: "+temp[4]+" Frühstück: "+temp[5]);
 		}
 		
 		//generieren eines „ListAdapter“ für die „ListView“
@@ -51,6 +52,7 @@ public class Lernplan extends Activity
 		//ListView aus layout holen und ihr den Adapter übergeben
 	    final ListView lv = (ListView)findViewById(R.id.listView1);
 	    lv.setAdapter(adapter);
+	    
 	    
 	}
 	
