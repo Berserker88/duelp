266bae84cff93ba9b5c6741faab79b2ba3f24789
Jannis Raiber <jannis.raiber@gmx.de>
2013-01-03 16:08:18 +0100

    Refixed detailview and enabled multi-selection in listview...

--- app/src/de/jasiflak/duelp/Faecher.java
+++ app/src/de/jasiflak/duelp/Faecher.java
@@ -1,6 +1,7 @@
 package de.jasiflak.duelp;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.ListActivity;
@@ -12,7 +13,10 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 //import android.widget.ListView;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
 
 public class Faecher extends ListActivity 
 {
@@ -21,6 +25,9 @@ public class Faecher extends ListActivity
 	
 	public void onCreate(Bundle savedInstanceState)
 	{
+		
+		Log.i("Debug","Create test...");
+
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.faecher_layout);
 		
@@ -29,8 +36,27 @@ public class Faecher extends ListActivity
 
 		setListAdapter(new Faecher_Apdapter(this, MOBILE_OS));
 		
+		//Checkbox
+		final CheckBox chkBox = (CheckBox) findViewById(R.id.chkbox);
+		 
+		OnClickListener checkBoxListener;
+		checkBoxListener =new OnClickListener()
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				if(chkBox.isChecked())
+				{
+					Log.i("Debug","Checkbox is checked..");	
+				}	
+				else
+				{
+					Log.i("Debug","Checkbox is checked..");	
+				}
+			};
+		};
 		
-		
+
 		//DATASOURCE
 		/*ArrayList<String> valueList = new ArrayList<String>();
 		for (int i = 0; i < 10; i++)
@@ -38,30 +64,25 @@ public class Faecher extends ListActivity
 			valueList.add("value" + i);
 		}*/
 
-	 
 		//LIST
 
-		
-		
 		/*ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
 	    final ListView lv = (ListView)findViewById(R.id.listView1);
 	    lv.setAdapter(adapter);	*/
-	    
-	    
-	    //ONCLICK
-	   /* lv.setOnItemClickListener(new OnItemClickListener()
-	    {
-	    	@Override
-	    	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3)
-	    	{
-	    		Intent intent = new Intent();
-	    		intent.setClassName(getPackageName(), getPackageName()+".FaecherDetail");
-	    		intent.putExtra("selected", lv.getAdapter().getItem(arg2).toString());
-	    		startActivity(intent);
-	    	}
-	    });*/
-	    
-	    
-	    
+
 	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id)
+	{
+		Log.i("Debug","item select....");
+		Intent intent = new Intent();
+		intent.setClassName(getPackageName(), getPackageName()+".FaecherDetail");
+		intent.putExtra("selected",MOBILE_OS[position]);
+		startActivity(intent);
+	}
+	
+
+  
+	    
 }
