b20b7cf6497050ed97842483afb1f1289c41b3b1
Jannis Raiber <jannis.raiber@gmx.de>
2013-01-05 23:54:39 +0100

    DatePicker finally works..

--- app/src/de/jasiflak/duelp/FaecherDetail.java
+++ app/src/de/jasiflak/duelp/FaecherDetail.java
@@ -3,7 +3,8 @@ package de.jasiflak.duelp;
 import java.util.Calendar;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -11,13 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TextView.*;
 
 
 public class FaecherDetail extends Activity
 {
-	
+
+	static final int DATE_DIALOG_ID = 777;
+
 	private int year;
 	private int month;
 	private int day;
@@ -27,23 +28,20 @@ public class FaecherDetail extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.faecher_detail_layout);
+
+	
+		//DateEditText...
+		 EditText dateEditText = (EditText) findViewById(R.id.txtDatum);
 		
-		Intent intent = getIntent();
-		//((TextView)(findViewById(R.id.textView1))).setText("Es wurde "+intent.getStringExtra("selected")+ " gewählt!");
+		dateEditText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	 showDialog(DATE_DIALOG_ID);
+            }
+        });
+
 		
 		//Set current date to label
 		setCurrentDateOnView();
-		
-		//DateEditText...
-		EditText dateEditText = (EditText) findViewById(R.id.txtDatum);
-		dateEditText.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-					Log.i("Debug","Date textField clicked");	  	 
-            }
-        });
-		
-		
 		
 		 //Save Button
 		 final Button button = (Button) findViewById(R.id.btnSave);
@@ -67,9 +65,10 @@ public class FaecherDetail extends Activity
          });
 	}
 	
+	
+/*----------Date Picker Methods--------------------------*/	
 	public void setCurrentDateOnView() {
 
-		EditText dateEditText = (EditText) findViewById(R.id.txtDatum);
 
 		final Calendar c = Calendar.getInstance();
 		year = c.get(Calendar.YEAR);
@@ -77,15 +76,46 @@ public class FaecherDetail extends Activity
 		day = c.get(Calendar.DAY_OF_MONTH);
 
 		// set current date into textview
+		 EditText dateEditText = (EditText) findViewById(R.id.txtDatum); 
+
 		dateEditText.setText(new StringBuilder()
 				// Month is 0 based, just add 1
 		.append(day).append(".").append(month + 1).append(".").append(year)
 				.append(" "));
 
-		// set current date into datepicker
-		//dpResult.init(year, month, day, null);
-
 	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DATE_DIALOG_ID:
+			// set date picker as current date
+			return new DatePickerDialog(this, datePickerListener, year, month,
+					day);
+		}
+		return null;
+	}
+
+	private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+
+		// when dialog box is closed, below method will be called.
+		public void onDateSet(DatePicker view, int selectedYear,
+				int selectedMonth, int selectedDay) {
+			year = selectedYear;
+			month = selectedMonth;
+			day = selectedDay;
+
+			// set current date into textview
+			EditText dateEditText = (EditText) findViewById(R.id.txtDatum); 
+			dateEditText.setText(new StringBuilder()
+					// Month is 0 based, just add 1
+			.append(day).append(".").append(month + 1).append(".").append(year)
+					.append(" "));
+
+			// set current date into datepicker
+
+		}
+	};
 
 	
 	
